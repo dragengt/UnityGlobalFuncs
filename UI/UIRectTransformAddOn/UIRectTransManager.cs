942387877486dba1d0d78f7a44de40acfc1feaf5
danteAtHome <664847089@qq.com>
2017-03-29 22:16:37 +0800

    扩展方法：RectTransform现在可以很好地指定多个层级之下的子物体，然后快速获得而无需一个个child去get

---
+++ UI/UIRectTransformAddOn/UIRectTransManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Game.UI
+{
+	public static class UIRectTransManager 
+	{
+        //为RectTransform增加一个扩展函数
+        /// <summary>
+        /// 根据id获得对应的rectTransform
+        /// </summary>
+        public static RectTransform GetChildById(this RectTransform parent, string childId)
+        {
+            UIRectTransExtend rectTransManager = parent.GetComponent<UIRectTransExtend>();
+            RectTransform childGot = null;
+
+            //没有管理器就null：
+            if (rectTransManager == null)
+            {
+                return null;
+            }
+
+            //--尝试获取child
+            rectTransManager.uiChildren.TryGetValue(childId, out childGot);
+            return childGot;
+        }
+        
+	}
+}
