5699bd522780d1a0f0a5c3c6ab8ea15f4affde7e
danteAtHome <664847089@qq.com>
2017-03-30 16:15:54 +0800

    修改UIRectTrans扩展器的名字。Manager可以被添加到UI物体身上

---
+++ UI/UIRectTransformAddOn/UIRectTransExtender.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Game.UI
+{
+	public static class UIRectTransExtender 
+	{
+        //为RectTransform增加一个扩展函数
+        /// <summary>
+        /// 根据id获得对应的rectTransform
+        /// </summary>
+        public static RectTransform GetChildById(this RectTransform parent, string childId)
+        {
+            UIRectTransManager rectTransManager = parent.GetComponent<UIRectTransManager>();
+            RectTransform childGot = null;
+
+            //没有管理器就null：
+            if (rectTransManager == null)
+            {
+                return null;
+            }
+            else
+            {
+                //--尝试获取child
+                rectTransManager.GetChildById(childId);
+                return childGot;
+            }
+        }
+        
+	}
+}
--- UI/UIRectTransformAddOn/UIRectTransManager.cs
+++ UI/UIRectTransformAddOn/UIRectTransManager.cs
@@ -1,29 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Game.UI
 {
-	public static class UIRectTransManager 
+    [RequireComponent(typeof(RectTransform))]
+    /// <summary>
+    /// 用于增强RectTransform管理子UI的行为：将这个脚本添加到对应的UI上，并设置《string，transform》，则RectTransform将可以获得对应string下的transform
+    /// </summary>
+	public class UIRectTransManager : MonoBehaviour 
 	{
+        [Tooltip("link to child with id")]
+        /// <summary>
+        /// 用于管理子物体的Dict
+        /// </summary>
+        [SerializeField]Dictionary<string, RectTransform> m_uiChildren = new Dictionary<string, RectTransform>();
+
+        /// <summary>
+        /// 获得用于管理子物体的Dict：只读
+        /// </summary>
+        public Dictionary<string, RectTransform> uiChildren { get { return m_uiChildren; } }
+
         //为RectTransform增加一个扩展函数
         /// <summary>
         /// 根据id获得对应的rectTransform
         /// </summary>
-        public static RectTransform GetChildById(this RectTransform parent, string childId)
+        public RectTransform GetChildById(string childId)
         {
-            UIRectTransExtend rectTransManager = parent.GetComponent<UIRectTransExtend>();
             RectTransform childGot = null;
 
-            //没有管理器就null：
-            if (rectTransManager == null)
-            {
-                return null;
-            }
-
             //--尝试获取child
-            rectTransManager.uiChildren.TryGetValue(childId, out childGot);
+            uiChildren.TryGetValue(childId, out childGot);
             return childGot;
         }
         
 	}
+
 }
