b17bb7efc67c404e22d8c860ca508951eda22e8e
danteAtHome <664847089@qq.com>
2017-04-17 22:51:03 +0800

    防止了Drag时Click的行为:土方法，判断前后位置关系，且是否还在UI上

--- UI/UIDialog/GameUIDragable.cs
+++ UI/UIDialog/GameUIDragable.cs
@@ -14,12 +14,19 @@ namespace Game.UI.Component
         /// </summary>
         [SerializeField]uint m_alignToGrid = 0;
 
+        [Tooltip("在拖拽时是否中断click的响应")]
+        /// <summary>
+        /// 在拖拽时是否中断click的响应
+        /// </summary>
+        [SerializeField]bool m_stopClickWhenDrag;
+
         RectTransform m_rectTransform;
 
+        //开始拖拽时UI的位置：
         Vector2 m_rectTransAnchoredPos;
 
         //开始拖拽时的触点位置
-        Vector2 m_DragStartPos;
+        Vector2 m_startDragPos;
 
         /// <summary>
         /// 是否是有效的拖拽行为：只有当从当前物体开始点击，才能在Drag中生效；DragUp之后取消标记
@@ -48,7 +55,7 @@ namespace Game.UI.Component
         public void OnPointerDown(PointerEventData data)
         {
             //开始拖拽时记录鼠标位置
-            m_DragStartPos = data.position;
+            m_startDragPos = data.position;
             //记录当前Rect位置
             m_rectTransAnchoredPos = m_rectTransform.anchoredPosition;
             //允许拖拽
@@ -60,14 +67,14 @@ namespace Game.UI.Component
         {
             if (!m_isValidDrag)
                 return;
-
+            
             //新计算方法：
             //获得移动差值
-            Vector2 posDelta = data.position - m_DragStartPos;
+            Vector2 posDelta = data.position - m_startDragPos;
 
             Vector2 newPos = m_rectTransAnchoredPos + posDelta;
 
-            //否则予以移动：
+            //予以移动：
             //--更新移动视图
             m_rectTransform.anchoredPosition = newPos;
         }
@@ -75,15 +82,26 @@ namespace Game.UI.Component
         //拖拽完成
         public void OnPointerUp(PointerEventData data)
         {
-            //标记移动结束：
-            m_isValidDrag = false;
-
-            if (m_alignToGrid > 0)
+            if (m_isValidDrag)
             {
+                //标记移动结束：
+                m_isValidDrag = false;
+
                 Vector2 vPos = m_rectTransform.anchoredPosition;
-                vPos.x = (int)vPos.x / m_alignToGrid * m_alignToGrid;
-                vPos.y = (int)vPos.y / m_alignToGrid * m_alignToGrid;
-                m_rectTransform.anchoredPosition = vPos;
+                if (m_alignToGrid > 0)
+                {
+                    vPos.x = (int)vPos.x / m_alignToGrid * m_alignToGrid;
+                    vPos.y = (int)vPos.y / m_alignToGrid * m_alignToGrid;
+                    m_rectTransform.anchoredPosition = vPos;
+                }
+
+                //再发起一次click事件的响应：
+                if (m_stopClickWhenDrag
+                    && (data.position != m_startDragPos)
+                    && MyInput.IsMouseOnUI()) 
+                {
+                    GetComponent<IPointerClickHandler>().OnPointerClick(data);
+                }
             }
         }
 
